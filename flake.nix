ede5dee8b4453b805caf01326b7fad75856c49db
Hraban <hraban@0brg.net>
2026-07-05 19:57:46 -0400

    build: move into flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
+  inputs.systems.url = "github:nix-systems/default";
+
+  outputs = { self, flake-parts, systems, ... }@inputs: flake-parts.lib.mkFlake { inherit inputs; } {
+    systems = import systems;
+    perSystem = { lib, pkgs, ... }: {
+      packages.default = pkgs.stdenv.mkDerivation {
+        name = "SKF";
+        src = lib.cleanSource ./.;
+        buildInputs = with pkgs ; [ SDL2 sdl2-compat SDL2_image SDL2_mixer libwebp libtiff ];
+        buildPhase = ''
+          cc -o nob nob.c
+          ./nob
+        '';
+        installPhase = ''
+          mkdir -p $out/bin
+          cp ./SKF $out/bin
+        '';
+      };
+    };
+  };
+}
